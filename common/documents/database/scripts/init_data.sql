22712d1ff6bc3457b2dc5c2d320fa1910713bc92
idkbj <idkbj@osci.kr>
2013-10-02 16:11:35 +0900

    role_tbl, user_group_tbl, users_tbl sample data 추가.

--- common/documents/database/scripts/init_data.sql
+++ common/documents/database/scripts/init_data.sql
@@ -10,3 +10,19 @@ INSERT INTO peacock.mon_factor_tbl(MON_FACTOR_ID,MON_FACTOR_NAME,MON_FACTOR_UNIT
 VALUES ('FACTOR_004','FREE_MEMORY','KB','Total free system memory',1,NOW(),1,NOW());
 INSERT INTO peacock.mon_factor_tbl(MON_FACTOR_ID,MON_FACTOR_NAME,MON_FACTOR_UNIT,MON_FACTOR_DESC,REG_USER_ID,REG_DT,UPD_USER_ID,UPD_DT)
 VALUES ('FACTOR_005','USED_MEMORY','KB','Total used system memory',1,NOW(),1,NOW());
+
+INSERT INTO `peacock`.`roles_tbl` (`ROLE_ID`, `ROLE_NAME`, `PERMISSION`, `REG_DT`) VALUES ('1', 'admin', 'all', NOW());
+INSERT INTO `peacock`.`roles_tbl` (`ROLE_ID`, `ROLE_NAME`, `PERMISSION`, `REG_DT`) VALUES ('2', 'role2', 'write', NOW());
+
+INSERT INTO `peacock`.`users_tbl` (`USER_ID`, `ROLE_ID`, `LOGIN_ID`, `HASHED_PASSWD`, `USER_NAME`, `DEPT_NAME`, `EMAIL`, `IS_ADMIN`, `STATUS`, `REG_DT`) 
+VALUES ('1', '1', 'idkbj', '1234', '권봉진', '개발팀', 'idkbj@naver.com', '1', '1', NOW());
+INSERT INTO `peacock`.`users_tbl` (`USER_ID`, `ROLE_ID`, `LOGIN_ID`, `HASHED_PASSWD`, `USER_NAME`, `DEPT_NAME`, `EMAIL`, `IS_ADMIN`, `STATUS`, `REG_DT`) 
+VALUES ('2', '1', 'abcd11', '1111', '홍길동', '개발팀', '111@naver.com', '0', '1', NOW());
+INSERT INTO `peacock`.`users_tbl` (`USER_ID`, `ROLE_ID`, `LOGIN_ID`, `HASHED_PASSWD`, `USER_NAME`, `DEPT_NAME`, `EMAIL`, `IS_ADMIN`, `STATUS`, `REG_DT`) 
+VALUES ('3', '1', 'test', '1111', '김철수', '개발팀', 'test@naver.com', '0', '1', NOW());
+INSERT INTO `peacock`.`users_tbl` (`USER_ID`, `ROLE_ID`, `LOGIN_ID`, `HASHED_PASSWD`, `USER_NAME`, `DEPT_NAME`, `EMAIL`, `IS_ADMIN`, `STATUS`, `REG_DT`) 
+VALUES ('4', '1', '1111', '1111', '1111', '개발팀', '111@naver.com', '0', '1', NOW());
+
+
+INSERT INTO `peacock`.`user_group_tbl` (`GROUP_ID`, `GROUP_NAME`, `DESCRIPTION`, `REG_DT`) VALUES ('1', 'osc-employee', 'Open Source Consulting', NOW());
+INSERT INTO `peacock`.`user_group_tbl` (`GROUP_ID`, `GROUP_NAME`, `DESCRIPTION`, `REG_DT`) VALUES ('2', 'ncia', '...', NOW());
